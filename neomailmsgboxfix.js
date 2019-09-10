3d02453a33f9d67712f5c3eb3f094272d5b61108
richardscoot <37322700+richardscoot@users.noreply.github.com>
2019-09-10 13:03:01 +0800

    Create neomailmsgboxfix.js

---
+++ neomailmsgboxfix.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Neomail - Add Message Box
+// @namespace    richardscoot@Clraik
+// @version      1.0
+// @description  Fixes the message box that went disappear sometimes when you are trying to send a neomail
+// @author       richardscoot
+// @match        *www.neopets.com/neomessages.phtml?*type=send*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var childno = 3;
+    if (window.location.href.indexOf("reply_message_id=") > -1) {
+        childno = 4;
+    }
+    var msgboxel = $('#content > table > tbody > tr > td.content > form > table > tbody > tr:nth-child(' + childno + ') > td:nth-child(2)');
+    if (msgboxel.children('textarea').length == 0) {
+        msgboxel.append('<textarea name="message_body" id="message_body" style="width: 600px; height: 100px; background-color: #d4f3ff;"></textarea>');
+    }
+})();
