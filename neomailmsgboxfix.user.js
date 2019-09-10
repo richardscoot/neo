29c840e832ef801341593ec1b66a7bba3079146f
richardscoot <37322700+richardscoot@users.noreply.github.com>
2019-09-10 14:48:18 +0800

    Update neomailmsgboxfix.user.js

--- neomailmsgboxfix.user.js
+++ neomailmsgboxfix.user.js
@@ -7,13 +7,4 @@
 // @version       1.0
 // ==/UserScript==
 
-(function() {
-    var childno = 3;
-    if (window.location.href.indexOf("reply_message_id=") > -1) {
-        childno = 4;
-    }
-    var msgboxel = $('#content > table > tbody > tr > td.content > form > table > tbody > tr:nth-child(' + childno + ') > td:nth-child(2)');
-    if (msgboxel.children('textarea').length == 0) {
-        msgboxel.append('<textarea name="message_body" id="message_body" style="width: 600px; height: 100px; background-color: #d4f3ff;"></textarea>');
-    }
-})();
+(function(){var childno=3;if(window.location.href.indexOf("reply_message_id=")>-1){childno=4}var msgboxel=$('#content > table > tbody > tr > td.content > form > table > tbody > tr:nth-child('+childno+') > td:nth-child(2)');if(msgboxel.children('textarea').length==0){msgboxel.append('<textarea name="message_body" id="message_body" style="width: 600px; height: 100px; background-color: #d4f3ff;"></textarea>')}})();
